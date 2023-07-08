1f0c7bd9c0b752623f0a45d79a8bb39d7c4d3546
hide1102 <136874206+hide1102@users.noreply.github.com>
2023-07-08 20:17:51 +0900

    TimeCount3

--- Assets/Script/Data.cs
+++ Assets/Script/Data.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Data : MonoBehaviour
 {
     public float score;
+    public float elapsedTime;
     // Start is called before the first frame update
     void Start()
     {
